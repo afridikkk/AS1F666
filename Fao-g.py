b37d16f4329972e12c690c3d966fb688de2268e7
afridikkk <101248927+afridikkk@users.noreply.github.com>
2022-08-29 20:29:56 +0500

    Delete Fao-g.py

--- Fao-g.py
+++
@@ -1,8 +0,0 @@
-import os,platform
-os.system('git pull')
-
-aarch64=platform.architecture()[0]
-if bsn=="32bit":
-    print('Sorry Update Your Phone...')
-elif aarch64=="64bit":
-    __import__("aarch64")
